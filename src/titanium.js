f4409df1cf08b58243a9d740f611376d18c3419d
Tony Lukasavage <anthony.lukasavage@gmail.com>
2013-12-30 14:57:30 -0500

    make node.js compatible console functions for easy reporter porting

--- src/titanium.js
+++ src/titanium.js
@@ -1,4 +1,13 @@
+/**
+ * NOTES
+ *
+ * - In these notes, I include "log()" when I refer to "Ti.API calls".
+ * - "◦" and "✓" character are handled inconsistently by Ti.API calls, used plain normal characters instead.
+ * - Leading whitespace is trimmed in Ti.API calls. A workaround is to wrap the whitespace with a color.
+ *
+ */
 
+// Create a titanium compatible reporter based on spec
 require.register("reporters/ti-spec.js", function(module, exports, require){
 
 /**
@@ -6,7 +15,6 @@ require.register("reporters/ti-spec.js", function(module, exports, require){
  */
 
 var Base = require('./base'),
-	cursor = Base.cursor,
 	color = Base.color;
 
 /**
@@ -30,62 +38,59 @@ function TiSpec(runner) {
 		indents = 0,
 		n = 0;
 
+	function CR() {
+		return '\u001b[2K' + '\u001b[0G';
+	}
+
+	function NL() {
+		log(color('pass', ''));
+	}
+
+	function upOne() {
+		return '\u001b[1F' + CR();
+	}
+
 	function indent() {
 		return new Array(indents).join('  ');
 	}
 
+	function log(msg) {
+		console.log(CR() + msg);
+	}
+
 	runner.on('start', function(){
-		console.log();
+		NL();
 	});
 
 	runner.on('suite', function(suite){
 		++indents;
-		//console.log(color('suite', '%s%s'), indent(), suite.title);
-		console.log(color('suite', indent() + suite.title));
+		log(color('suite', indent() + suite.title));
 	});
 
 	runner.on('suite end', function(suite){
 		--indents;
-		if (1 === indents) { console.log(); }
+		if (1 === indents) { NL(); }
 	});
 
 	runner.on('test', function(test){
-		//process.stdout.write(indent() + color('pass', '  ◦ ' + test.title + ': '));
+		log(color('pass', indent() + '  o ' + test.title + ': '));
 	});
 
 	runner.on('pending', function(test){
-		//var fmt = indent() + color('pending', '  - %s');
-		//console.log(fmt, test.title);
-		console.log(indent() + color('pending', '  - ' + test.title));
+		log(color('pending', indent() + '  - ' + test.title));
 	});
 
 	runner.on('pass', function(test){
 		if ('fast' === test.speed) {
-			console.log(indent() + color('checkmark', '  ' + Base.symbols.ok) + color('pass', ' ' + test.title + ' '));
+			log(upOne() + color('checkmark', indent() + '  +') + color('pass', ' ' + test.title + ' '));
 		} else {
-			console.log(indent() + color('checkmark', '  ' + Base.symbols.ok) + color('pass', ' ' + test.title + ' ') +
+			log(upOne() + color('checkmark', indent() + '  +') + color('pass', ' ' + test.title + ' ') +
 				color(test.speed, '(' + test.duration + 'ms)'));
 		}
-		// if ('fast' == test.speed) {
-		// 	var fmt = indent()
-		// 		+ color('checkmark', '  ' + Base.symbols.ok)
-		// 		+ color('pass', ' %s ');
-		// 	cursor.CR();
-		// 	console.log(fmt, test.title);
-		// } else {
-		// 	var fmt = indent()
-		// 		+ color('checkmark', '  ' + Base.symbols.ok)
-		// 		+ color('pass', ' %s ')
-		// 		+ color(test.speed, '(%dms)');
-		// 	cursor.CR();
-		// 	console.log(fmt, test.title, test.duration);
-		// }
 	});
 
 	runner.on('fail', function(test, err){
-		// cursor.CR();
-		// console.log(indent() + color('fail', '  %d) %s'), ++n, test.title);
-		console.log(indent() + color('fail', '  ' + (++n) + ') ' + test.title));
+		log(color('fail', upOne() + indent() + '  ' + (++n) + ') ' + test.title));
 	});
 
 	runner.on('end', function() {
@@ -104,9 +109,27 @@ TiSpec.prototype.constructor = TiSpec;
 
 }); // module: reporters/ti-spec.js
 
-if (typeof Titanium !== 'undefined') {
+// stub location that mocha uses
+global.location = {};
 
-	// stub location
-	global.location = {};
+// Override the console functions with node.js-style formatting. This allows us to use some of mocha's existing
+// reporters with only a few modifications, like I do with spec.
+var console = {};
+var types = ['info','log','error','warn','trace'];
+var LINE_RESET = '\u001b[2K' + '\u001b[0G';
+for (var i = 0; i < types.length; i++) {
+  var type = types[i];
+  console[type] = function() {
 
+  	var args = Array.prototype.slice.call(arguments, 0);
+  	if (args.length === 0) {
+  		args.push(LINE_RESET);
+  	} else {
+  		// Clear the existing line of text using ANSI codes, get rid of those pesky [INFO] prefixes
+  		args[0] = LINE_RESET + args[0].toString().split(/(?:\r\n|\n|\r)/).join('\n' + LINE_RESET);
+  	}
+
+  	// Use the util.js format() port to get node.js-like console functions
+    Ti.API.log(type === 'log' ? 'info' : type, require('titanium/util').format.apply(this, args));
+  };
 }
